fa826977bb4d3d2344023ec1b672123fb28dd9cd
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2013-12-02 20:40:39 +0100

    int controleurs (oubli)

---
+++ src/Sio/SemiBundle/Tests/Controller/AdminControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sio\SemiBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdminControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/index');
+    }
+
+}
