ef4db76061bbffb879fec6f0b2bd8301810682f6
tchoisy <choisy.tommy@gmail.com>
2013-12-08 18:41:42 +0100

    avance dans renseignement cle (pb)

--- src/Sio/SemiBundle/Controller/DefaultController.php
+++ src/Sio/SemiBundle/Controller/DefaultController.php
@@ -5,11 +5,12 @@ namespace Sio\SemiBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/list-seminaire")
+     * @Route("/list-seminaire", name="liste_semi")
      * @Template()
      */
     public function indexAction()
@@ -17,5 +18,38 @@ class DefaultController extends Controller
     	$em = $this->getDoctrine()->getManager();
     	$seminaires = $em->getRepository('SioSemiBundle:Seminaire')->findAll(); 
         return array('seminaires' => $seminaires);
+    }
+    
+    /**
+     * @Route("/", name="connect_semi")
+     * @Template()
+     */
+    public function connectSemiAction(Request $request)
+    {
+        $defaultData = array();
+        $form = $this->createFormBuilder($defaultData)
+            ->add('cle_semi', 'text')
+            ->add('go','submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($request->getMethod() == 'POST') {
+            if ($form->isValid()) {
+                $data = $form->getData();
+                // TODO traitement pour savoir si la clé existe
+               $exist = $this->getDoctrine()
+                    ->getRepository('SioSemiBundle:Seminaire')
+                    ->existKey($form->get('cle_semi')->getData());
+                
+               if($exist){
+                   return $this->redirect($this->generateUrl('liste_semi'));
+               }else{
+                   return $this->redirect($this->generateUrl('connect_semi'));
+               }
+            }
+        }
+        return $this->render('SioSemiBundle:Default:connectSemi.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
