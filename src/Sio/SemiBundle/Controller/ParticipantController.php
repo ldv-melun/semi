8b2e5976da4e96a34359bed88b5dc78ddad6d12e
tchoisy <choisy.tommy@gmail.com>
2013-12-18 18:25:38 +0100

    formulaire profil TODO traitement & design

--- src/Sio/SemiBundle/Controller/ParticipantController.php
+++ src/Sio/SemiBundle/Controller/ParticipantController.php
@@ -7,17 +7,59 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
- * @Route("/participant")
+ * 
  */
 class ParticipantController extends Controller
 {
 	/**
-	 * @Route("/index")
+	 * @Route("/gestion/profil", name="gestion_profil")
 	 * @Template()
 	 */
-	public function indexAction()
+	public function gestionProfilAction()
 	{
-		return array();
+                $userCurrent = $this->getUser();
+                $form = $this->createFormBuilder($userCurrent)
+                    ->add('nom', 'text')
+                    ->add('prenom', 'text')
+                    ->add('mail', 'text',array(
+                        'read_only' => true,
+                    ))
+                    ->add('academie', 'entity', array(
+                        'class'    => 'SioSemiBundle:Academie',
+                        'property' => 'nom',
+                        'multiple' => false,
+                        'expanded' => false)
+                    )
+                    ->add('resFamiliale', 'text')
+                    ->add('resAdministrative', 'text')                    
+                    ->add('titre', 'choice', array(
+                        'choices'   => array(
+                            'professeur'   => 'Professeur',
+                            'ipr' => 'IA-IPR',
+                            'ien'   => 'IEN',
+                            'autre'   => 'Autre',
+                        ),
+                        'multiple'  => false,
+                        'expanded' => true,
+                    ))
+                    ->getForm();
+                
+                $request = $this->get('request');
+                // On vérifie qu'elle est de type POST
+               /* if ($request->getMethod() == 'POST') {
+                
+                    $form->bind($request);
+
+                    if ($form->isValid()) {
+
+                        $em = $this->getDoctrine()->getManager();
+                        $em->persist($userCurrent);
+                        $em->flush();
+
+                        return $this->redirect();
+                    }
+                }*/
+		return array("form" => $form->createView());
 	}
 	
 }
