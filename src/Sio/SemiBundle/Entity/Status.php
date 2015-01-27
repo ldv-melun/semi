407b90ed283b591da1d781d1cbcfa90c9f1a1bed
Jérôme Pros <jerome.pros@gmail.com>
2015-01-27 10:24:53 +0100

    Ajout de l'entité Status.

---
+++ src/Sio/SemiBundle/Entity/Status.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Status
+ *
+ * @ORM\Table(name="semi_status")
+ * @ORM\Entity
+ */
+class Status {
+    
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=50, nullable=true)
+     */
+    private $status;
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * Set status
+     *
+     * @param string $status
+     * @return SeminarStatus
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
