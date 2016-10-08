fd3cba8b912cedd884bd290b7707ad3a6d982483
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2016-10-08 09:10:20 +0200

    refactoring  code  js (semi.js) et reformulation "participant" au lieu de "inscrit"

--- web/bundles/siosemi/js/semi.js
+++ web/bundles/siosemi/js/semi.js
@@ -16,40 +16,12 @@ function inscrSeance(idSeance, heureDeb, description, checked) {
       alert("Error : " + msg);
     },
     success: function (data) {
-      // console.log(data.statMeeting[0].free);  
-      // refresh stats : see meetings.html.twig 
-      for (var i = 0; i < data.statMeeting.length; i++) {
-        var stat = data.statMeeting[i].free + " / " + data.statMeeting[i].maxSeats;
-        var actual = $('#' + data.statMeeting[i].id).text().trim();
-        var free = 1 * data.statMeeting[i].free;
-        // console.log(data.statMeeting[i].free);
-        //console.log("["+stat+"]["+actual+"]"+ " free=["+free+"]");
-        if (stat != actual) {
-          $('#' + data.statMeeting[i].id).css("background-color", "lightyellow");
-           // see meetings.html.twig           
-          $('#' + data.statMeeting[i].id).html(stat);
-        } else {          
-          $('#' + data.statMeeting[i].id).css("background-color", "");
-        }
-        if (free == 0){          
-          $('#' + data.statMeeting[i].id).css("background-color", "red");
-        } else if (free <= 10){          
-          $('#' + data.statMeeting[i].id).css("background-color", "coral");
-        }            
-      }
-      
-      // voir meetings.html.twig
-      if (data.statNbUsers > 1) {
-        $('#statNbUsers').html("<b>" + data.statNbUsers + "</b> personnes inscrites.");
-      } else {
-        $('#statNbUsers').html("<b>" + data.statNbUsers + "</b> personne inscrite.");
-      }
-      
-      // stat this user
+      updateStatMeetings(data);
+      // stat of this user
       $('#statNbSeanceInscr').html(data.statCurUser);
     }
   });
-}  
+}
 
 /**
  *  get stats of meeting for seminar in session of user (manager only)
@@ -65,31 +37,36 @@ function stateInscrSeance() {
       // alert("Error : " + msg);
     },
     success: function (data) {
-      // refresh stats : see meetings.html.twig 
-      for (var i = 0; i < data.statMeeting.length; i++) {
-        var stat = data.statMeeting[i].free + " / " + data.statMeeting[i].maxSeats;
-        var actual = $('#' + data.statMeeting[i].id).text().trim();
-        var free = 1 * data.statMeeting[i].free;
-        // console.log(data.statMeeting[i].free);
-        //console.log("["+stat+"]["+actual+"]"+ " free=["+free+"]");
-        if (stat != actual) {
-          $('#' + data.statMeeting[i].id).css("background-color", "lightyellow");
-          $('#' + data.statMeeting[i].id).html(stat);
-        } else {          
-          $('#' + data.statMeeting[i].id).css("background-color", "");
-        }
-        if (free == 0){          
-          $('#' + data.statMeeting[i].id).css("background-color", "red");
-        } else if (free <= 10){          
-          $('#' + data.statMeeting[i].id).css("background-color", "coral");
-        }        
-      }
-      // voir meetings.html.twig
-      if (data.statNbUsers > 1) {
-        $('#statNbUsers').html("<b>" + data.statNbUsers + "</b> personnes inscrites.");
-      } else {
-        $('#statNbUsers').html("<b>" + data.statNbUsers + "</b> personne inscrite.");
-      }      
+      updateStatMeetings(data);
     }
   });
-}  
+}
+
+
+function updateStatMeetings(data) {
+  // refresh stats : see meetings.html.twig 
+  for (var i = 0; i < data.statMeeting.length; i++) {
+    var stat = data.statMeeting[i].free + " / " + data.statMeeting[i].maxSeats;
+    var actual = $('#' + data.statMeeting[i].id).text().trim();
+    var free = 1 * data.statMeeting[i].free;
+    // console.log(data.statMeeting[i].free);
+    //console.log("["+stat+"]["+actual+"]"+ " free=["+free+"]");
+    if (stat != actual) {
+      $('#' + data.statMeeting[i].id).css("background-color", "lightyellow");
+      $('#' + data.statMeeting[i].id).html(stat);
+    } else {
+      $('#' + data.statMeeting[i].id).css("background-color", "");
+    }
+    if (free == 0) {
+      $('#' + data.statMeeting[i].id).css("background-color", "red");
+    } else if (free <= 10) {
+      $('#' + data.statMeeting[i].id).css("background-color", "coral");
+    }
+  }
+  // voir meetings.html.twig
+  if (data.statNbUsers > 1) {
+    $('#statNbUsers').html("<b>" + data.statNbUsers + "</b> participants.");
+  } else {
+    $('#statNbUsers').html("<b>" + data.statNbUsers + "</b> participant.");
+  }
+}
